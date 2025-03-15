fb78bc60677083e7cfdee344d022328404a9b274
beeena4 <febridkune54@gmail.com>
2025-03-15 16:22:56 +0700

    apdet

--- src/models/ArtificialFlower.java
+++ src/models/ArtificialFlower.java
@@ -1,10 +1,19 @@
 package models;
 
 public class ArtificialFlower extends Flower {
-    private String material;
+    private String material; // atribut private
 
     public ArtificialFlower(String nama, double harga, String kategori, String material) {
         super(nama, harga, kategori);
+        this.material = material;
+    }
+
+    // metode getter untuk material
+    public String getMaterial() {
+        return material;
+    }
+    // metode setter untuk material
+    public void setMaterial(String material) {
         this.material = material;
     }
 
--- src/models/FreshFlower.java
+++ src/models/FreshFlower.java
@@ -1,17 +1,18 @@
 package models;
 
 public class FreshFlower extends Flower {
-    private int dayFresh;
+    private int dayFresh; // atribut private
 
     public FreshFlower(String nama, double harga, String kategori, int dayFresh) {
         super(nama, harga, kategori);
         this.dayFresh = dayFresh;
     }
-
+    // metode getter untuk dayfresh
     public int getDayFresh() {
         return dayFresh;
     }
 
+    // metode setter untuk dayfresh
     public void setDayFresh(int dayFresh) {
         this.dayFresh = dayFresh;
     }
